c419045adce0e6b59d8ac58991977d8b352798b4
Michel Marconssin <m.marconssin@gmail.com>
2025-11-29 11:07:14 +0000

    Alterações

--- arvore.py
+++ arvore.py
@@ -1,3 +1,5 @@
+#Implementação das classes
+
 class Noh:
     def __init__(self, nome, idade):
         self.nome = nome
@@ -22,31 +24,64 @@ class Bst:
             else:
                 self._inserir_recursivo(atual.esq, novo_noh)
         else:
-            if atual.dir is None:
-                atual.dir = novo_noh
+            if novo_noh.idade > atual.idade:
+                if atual.dir is None:
+                    atual.dir = novo_noh
+                else:
+                    self._inserir_recursivo(atual.dir, novo_noh)
             else:
-                self._inserir_recursivo(atual.dir, novo_noh)
+                if novo_noh.nome < atual.nome:
+                    if atual.esq is None:
+                        atual.esq = novo_noh
+                    else:
+                        self._inserir_recursivo(atual.esq, novo_noh)
+                else:
+                    if atual.dir is None:
+                        atual.dir = novo_noh
+                    else:
+                        self._inserir_recursivo(atual.dir, novo_noh)
 
-            
+    def inOrder(self, atual):
+        if atual != None:
+            self.inOrder(atual.esq)
+            print(f"Nome: {atual.nome.ljust(10)} - Idade: {atual.idade}") 
+            self.inOrder(atual.dir)
+  
+    def preOrder(self, atual):
+        if atual != None:
+            print(f"Nome: {atual.nome.ljust(10)} - Idade: {atual.idade}")
+            self.preOrder(atual.esq)
+            self.preOrder(atual.dir)
+       
+    def posOrder(self, atual):
+        if atual != None:
+            self.posOrder(atual.esq)
+            self.posOrder(atual.dir)
+            print(f"Nome: {atual.nome.ljust(10)} - Idade: {atual.idade}")
 
+    def decOrder(self, atual):
+        if atual != None:
+            self.decOrder(atual.dir)
+            print(f"Nome: {atual.nome.ljust(10)} - Idade: {atual.idade}")
+            self.decOrder(atual.esq)
 
+    def largura(self, atual):
+        if atual != None:
+            print(f"Nome: {atual.nome.ljust(10)} - Idade: {atual.idade}")
+            self.largura(atual.esq)
+            self.largura(atual.dir)
 
-#    def buscar (self, x):
-#    def inOrder (self, x):
-#    def preOrder (self, x):
-#    def posOrder (self,x ):
-#    def decOrder (sefl, x):
-#    def largura (self, x):
+# Fim da implementação das classes
 
+# Início do fluxo principal
 
-print("Iniciando a árvore binária de busca...")
-
+#Populando uma lista de dicionários para usar como dados de entrada
 candidatos = [
     {"idade": 25, "nome": "pedro"},
     {"idade": 30, "nome": "marcos"},
     {"idade": 45, "nome": "marcelo"},
+    {"idade": 18, "nome": "scarlet"},
     {"idade": 18, "nome": "camila"},
-    {"idade": 22, "nome": "scarlet"},
     {"idade": 35, "nome": "matheus"},
     {"idade": 20, "nome": "thiago"},
     {"idade": 65, "nome": "joaquim"},
@@ -54,35 +89,27 @@ candidatos = [
     {"idade": 43, "nome": "julia"},
 ]
 
+#Criando a árvore binária e populando com a lista criada anteriormente
 arvore = Bst()
 for candidato in candidatos:
-    noh = Noh(candidato["nome"], candidato["idade"])
+    noh = Noh(candidato["nome"].lower(), candidato["idade"])
     arvore.inserir(noh)
 
+no_raiz = arvore.raiz
 
-print(arvore.raiz.nome)
-print(arvore.raiz.esq.nome)
-print(arvore.raiz.dir.nome)
-
-arvore_2 = Bst()
-
-arvore_2  = arvore.raiz.esq
-print(arvore_2.nome)
-
-
-
-
-    
-
-
-#print(candidatos[0])
-#print(candidatos[1])
-#print(candidatos[2])
-#print(candidatos[3])
-#print(candidatos[4])
-#print(candidatos[5])
-#print(candidatos[6])
-#print(candidatos[7])
-#print(candidatos[8])
-#print(candidatos[9])
-
+#Navegando na árvore binária criada
+print("In Order:")
+arvore.inOrder(no_raiz)
+print()
+print("Pre Order:")
+arvore.preOrder(no_raiz)
+print()
+print("Pos Order:")
+arvore.posOrder(no_raiz)
+print()
+print("Dec Order:")
+arvore.decOrder(no_raiz)
+print()
+print("Largura:")
+arvore.largura(no_raiz)
+print()
